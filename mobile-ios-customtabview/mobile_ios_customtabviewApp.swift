87d5f7cdd642b2344aaad92b1bb70275367ce0bb
Kiu Ai <aij914@gmail.com>
2021-02-19 22:00:07 +0800

    init

---
+++ mobile-ios-customtabview/mobile_ios_customtabviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_ios_customtabviewApp.swift
+//  mobile-ios-customtabview
+//
+//  Created by Alvin Ai on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct mobile_ios_customtabviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
